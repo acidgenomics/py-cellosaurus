2c3a97dcf2fac68d70fa3645d5c50c20f4a425fb
Michael Steinbaugh <mike@steinbaugh.com>
2026-06-07 06:52:41 -0400

    Add Sphinx+PyData docs scaffold and ReadTheDocs config
    - docs/conf.py: Sphinx with autodoc, autosummary, numpydoc, PyData theme,
      intersphinx to python/numpy/pandas docs.
    - docs/index.rst: Landing page with toctree → api.
    - docs/api.rst: autosummary table pointing at the package root.
    - .readthedocs.yaml: RTD build config (Python 3.12, sphinx fail_on_warning).
    - pyproject.toml: add [docs] optional-dependency group
      (sphinx, pydata-sphinx-theme, numpydoc, myst-parser).

---
+++ docs/conf.py
@@ -0,0 +1,29 @@
+"""Sphinx configuration for py-cellosaurus."""
+
+project = "cellosaurus"
+author = "Michael Steinbaugh"
+copyright = "Acid Genomics"  # noqa: A001
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "numpydoc",
+]
+autosummary_generate = True
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": False,
+    "show-inheritance": True,
+}
+napoleon_numpy_docstring = True
+napoleon_google_docstring = False
+html_theme = "pydata_sphinx_theme"
+html_theme_options = {
+    "github_url": "https://github.com/acidgenomics/py-cellosaurus",
+    "logo": {"text": "cellosaurus"},
+}
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "pandas": ("https://pandas.pydata.org/docs", None),
+}
