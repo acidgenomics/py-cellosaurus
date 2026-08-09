6272ed2b233513db10106734cc5afca5c73c99a9
Michael Steinbaugh <mike@steinbaugh.com>
2026-08-09 18:29:17 -0400

    2026-08-09 updates

--- docs/conf.py
+++ docs/conf.py
@@ -20,13 +20,14 @@ autodoc_default_options = {
 napoleon_numpy_docstring = True
 napoleon_google_docstring = False
 numpydoc_show_class_members = False
-html_theme = "pydata_sphinx_theme"
+html_theme = "acidgenomics"
+html_theme_path = ["_themes"]
 html_theme_options = {
-    "github_url": "https://github.com/acidgenomics/py-cellosaurus",
-    "logo": {"text": "cellosaurus"},
+    "sitesearch": "python.acidgenomics.com",
+    "repo_url": "https://github.com/acidgenomics/py-cellosaurus",
 }
-html_css_files = ["https://python.acidgenomics.com/css/sphinx.css"]
 html_show_sourcelink = False
+html_show_sphinx = False
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "pandas": ("https://pandas.pydata.org/docs", None),
